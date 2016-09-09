fba5efe6f8fd27dc31943b0da455aa659bc9411a
naiieandrade <andradenaiara@hotmail.com>
2016-09-09 15:08:19 -0300

    Applying the style and design technique in AboutApp class.

--- EmerGo/app/src/main/java/unlv/erc/emergo/controller/AboutApp.java
+++ EmerGo/app/src/main/java/unlv/erc/emergo/controller/AboutApp.java
@@ -1,6 +1,5 @@
 package unlv.erc.emergo.controller;
 
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -11,12 +10,12 @@ public class AboutApp extends Activity {
 
     private TextView informationAboutApp;
 
-
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.about_app);
-        String message= "\tEmerGo é um aplicativo que oferece a facilidade de " +
+
+        String messageAboutApp = "\tEmerGo é um aplicativo que oferece a facilidade de " +
                 "encontrar Unidades de Saúde mais próximas. Possui MODO EMERGÊNCIA, " +
                 "que traça a rota para uma Unidade de Saúde mais próxima, " +
                 "ligar para o SAMU, e envia uma mensagem pré definida com pedido de ajuda para " +
@@ -24,8 +23,7 @@ public class AboutApp extends Activity {
                 "\n\n\tTodas as funcionalidades em suas mãos, em apenas um aplicativo.";
 
         informationAboutApp = (TextView) findViewById(R.id.aboutApp);
-        informationAboutApp.setText(message);
+        informationAboutApp.setText(messageAboutApp);
 
     }
-
 }
