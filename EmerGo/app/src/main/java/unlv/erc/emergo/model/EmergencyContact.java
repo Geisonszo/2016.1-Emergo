38ebd050bcbc885799eb186555a176956eb1c57d
Gabriel Rodrigues <gabrielrodriguesfaria97@gmail.com>
2016-09-10 04:24:38 -0300

    Applying design and comments techniques - EmergencyContact

--- EmerGo/app/src/main/java/unlv/erc/emergo/model/EmergencyContact.java
+++ EmerGo/app/src/main/java/unlv/erc/emergo/model/EmergencyContact.java
@@ -1,5 +1,7 @@
 /**
- * EmergencyContatct.java
+ * Class name: EmergencyContact.java
+ *
+ * Purpouse: represent the data from an emergency contact signed in the Emergo.
  */
 
 package unlv.erc.emergo.model;
@@ -9,28 +11,62 @@ public class EmergencyContact {
     private String name;
     private String phone;
 
+
+    /**
+     * Empty constructor
+     */
+    public EmergencyContact() {
+
+    }
+
+
+    /**
+     * Emergency contacts constructor
+     * @param name
+     * @param phone
+     */
     public EmergencyContact(String name, String phone) {
         setName(name);
         setPhone(phone);
     }
 
-    public EmergencyContact(){
-
-    }
-
+    /**
+     * Get the name of this emergency contact instance.
+     *
+     * @return name;
+     */
     public String getName() {
+
         return name;
     }
 
+    /**
+     * Set the received name as name of this emergency contact instance.
+     *
+     * @param name
+     */
     public void setName(String name) {
+
         this.name = name;
     }
 
+    /**
+     * Get the phone of this emergency contact instance.
+     *
+     * @return phone;
+     */
     public String getPhone() {
+
         return phone;
     }
 
+    /**
+     * Set the received phone as phone of this emergency contact instance.
+     *
+     * @param phone
+     */
     public void setPhone(String phone) {
+
         this.phone = phone;
     }
 }
