aa703aed7dcd9139569a5bbf90967ecd742d11b4
Gabriel Rodrigues <gabrielrodriguesfaria97@gmail.com>
2016-09-10 13:42:57 -0300

    Applying design techniques to MaskHelperTest

--- EmerGo/app/src/androidTest/java/helper/MaskHelperTest.java
+++ EmerGo/app/src/androidTest/java/helper/MaskHelperTest.java
@@ -2,39 +2,39 @@ package helper;
 
 import junit.framework.TestCase;
 
-public class MaskHelperTest  extends TestCase{
+public class MaskHelperTest  extends TestCase {
+
     MaskHelper maskHelper = new MaskHelper();
 
-    public void testThatStringChanges(){
+    public void testThatStringChanges() {
 
         String name = "jujuba";
         String name1 = name;
         name1 = maskHelper.unmask(name);
         assertNotSame(name , name1);
-
     }
 
-    public void testIfDotDisapear(){
+    public void testIfDotDisapear() {
+
         String string1 = "leo.leo";
         String string2 = "leoleo";
         string1 = maskHelper.unmask(string1);
         assertEquals(string1 , string2);
     }
 
-    public void testIfReturnsVoid(){
+    public void testIfReturnsVoid() {
+
         boolean passou = true;
         String isVoid = "";
         String symbols = "././";
         symbols = maskHelper.unmask(symbols);
 
         if(symbols.compareTo(isVoid) == 0){
+
             assertTrue(passou);
-        }else{
+        } else {
+
             assertFalse(passou);
         }
     }
-
-
-
-
 }
