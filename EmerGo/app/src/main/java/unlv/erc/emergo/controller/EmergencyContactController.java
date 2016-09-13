9c9b6c24f5f2c8b192d4b665fe6b62b4546f7634
Gabriel Rodrigues <gabrielrodriguesfaria97@gmail.com>
2016-09-13 15:47:40 -0300

    Applying comments techniques on EmergencyContactController

--- EmerGo/app/src/main/java/unlv/erc/emergo/controller/EmergencyContactController.java
+++ EmerGo/app/src/main/java/unlv/erc/emergo/controller/EmergencyContactController.java
@@ -1,3 +1,10 @@
+/*
+ * Class: EmergencyContactController (.java)
+ *
+ * Porpouse: This class contais the objects and methods related to the user's emergency contacts.
+ */
+
+
 package unlv.erc.emergo.controller;
 
 import android.app.Activity;
@@ -44,6 +51,7 @@ public class EmergencyContactController extends Activity {
     private Integer idThirdContact = 3;
 
     EmergencyContactDao emergencyContactDao;
+
     final String ROUTETRACED = "Rota mais próxima traçada";
 
     public EmergencyContactController(){
@@ -120,7 +128,7 @@ public class EmergencyContactController extends Activity {
 
             public void onClick(View v) {
 
-                disableJustUpdateButton(nameFirstContact, phoneFirstContact, updateFirstContact,
+                disableUpdateButton(nameFirstContact, phoneFirstContact, updateFirstContact,
                         saveFirstContact);
                 saveFirstContact.setOnClickListener(new View.OnClickListener() {
 
@@ -199,7 +207,7 @@ public class EmergencyContactController extends Activity {
 
             public void onClick(View v) {
 
-                disableJustUpdateButton(nameSecondContact, phoneSecondContact, updateSecondContact,
+                disableUpdateButton(nameSecondContact, phoneSecondContact, updateSecondContact,
                         saveSecondContact);
                 saveSecondContact.setOnClickListener(new View.OnClickListener() {
 
@@ -279,7 +287,7 @@ public class EmergencyContactController extends Activity {
 
             public void onClick(View v) {
 
-                disableJustUpdateButton(nameThirdContact,phoneThirdContact,updateThirdContact,
+                disableUpdateButton(nameThirdContact,phoneThirdContact,updateThirdContact,
                         saveThirdContact);
                 saveThirdContact.setOnClickListener(new View.OnClickListener() {
 
@@ -323,7 +331,14 @@ public class EmergencyContactController extends Activity {
         });
     }
 
-    private boolean signInFirstContact(){
+    /**
+     * This method is used to sign the first contact in the system, returns a bollean that informs
+     * if the sign was sucessfull.
+     * @return valid
+     */
+
+    private boolean signInFirstContact() {
+
         boolean sucess = true;
         boolean valid = false;
         if(checksName(nameFirstContact.getText().toString()) == false) {
@@ -338,7 +353,7 @@ public class EmergencyContactController extends Activity {
                 valid = true;
                 nameFirstContact.setEnabled(false);
                 phoneFirstContact.setEnabled(false);
-                disableOptionsUpdate(saveFirstContact,updateFirstContact,deleteFirstContact);
+                disableSaveButton(saveFirstContact,updateFirstContact,deleteFirstContact);
             } else {
 
                 showMessage("Contato de Emergência Não Cadastrado! Tente Novamente");
@@ -348,8 +363,13 @@ public class EmergencyContactController extends Activity {
         return valid;
     }
 
+    /**
+     * This method is used to sign the second contact in the system, returns a bollean that informs
+     * if the sign was sucessfull.
+     * @return valid
+     */
 
-    private boolean signInSecondContact(){
+    private boolean signInSecondContact() {
         boolean sucess = true;
         boolean valid = false;
 
@@ -364,7 +384,7 @@ public class EmergencyContactController extends Activity {
                     valid = true;
                     nameSecondContact.setEnabled(false);
                     phoneSecondContact.setEnabled(false);
-                    disableOptionsUpdate(saveSecondContact,updateSecondContact,deleteSecondContact);
+                    disableSaveButton(saveSecondContact,updateSecondContact,deleteSecondContact);
                 } else {
                     showMessage("Contato de Emergência Não Cadastrado! Tente Novamente");
                     valid = true;
@@ -374,7 +394,13 @@ public class EmergencyContactController extends Activity {
         return valid;
     }
 
-    private boolean signInthirdContact(){
+    /**
+     * This method is used to sign the third contact in the system, returns a bollean that informs
+     * if the sign was sucessfull.
+     * @return valid
+     */
+
+    private boolean signInthirdContact() {
 
         boolean sucess = true;
         boolean valid = false;
@@ -393,7 +419,7 @@ public class EmergencyContactController extends Activity {
                     valid = true;
                     nameThirdContact.setEnabled(false);
                     phoneThirdContact.setEnabled(false);
-                    disableOptionsUpdate(saveThirdContact,updateThirdContact,deleteThirdContact);
+                    disableSaveButton(saveThirdContact,updateThirdContact,deleteThirdContact);
                 } else {
 
                     showMessage("Contato de Emergência Não Cadastrado! Tente Novamente");
@@ -404,16 +430,29 @@ public class EmergencyContactController extends Activity {
         return valid;
     }
 
+    /**
+     * This method is used to update any of the three emergency contacts already signed on EmerGo.
+     *
+     * @param id
+     * @param name
+     * @param phone
+     * @param save
+     * @param update
+     * @param delete
+     */
+
     private void updateContact(Integer id,EditText name,EditText phone,Button save,Button update,
-                               Button delete){
+                               Button delete) {
+
         boolean sucess = true;
-        if(checksName(name.getText().toString()) == false){
+
+        if(checksName(name.getText().toString()) == false) {
 
             nameContact = name.getText().toString();
             phoneContact = phone.getText().toString();
 
             sucess = emergencyContactDao.updateEmergencyContact(id,nameContact,phoneContact);
-            if(sucess == true){
+            if(sucess == true) {
 
                 showMessage("Contato de Emergência Alterado Com Sucesso!");
                 save.setVisibility(View.VISIBLE);
@@ -427,9 +466,20 @@ public class EmergencyContactController extends Activity {
         }
     }
 
+    /**
+     * This method is used to exclude any of the three emergency contacts signed on system.
+     *
+     * @param nameContact
+     * @param phoneContact
+     * @param save
+     * @param id
+     * @param update
+     * @param delete
+     */
+
     private void deleteContact(final EditText nameContact, final EditText phoneContact,
                                final Button save, final Integer id, final Button update,
-                               final Button delete){
+                               final Button delete) {
 
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
 
@@ -464,7 +514,15 @@ public class EmergencyContactController extends Activity {
         builder.show();
     }
 
-    private boolean checksName(String nameUser){
+    /**
+     * This method is used to check if the name informed by the user is valid.
+     * If the name is valid, false is returned.
+     *
+     * @param nameUser
+     * @return boolean
+     */
+
+    private boolean checksName(String nameUser) {
 
         final int MINIMUM = 3;
 
@@ -487,25 +545,57 @@ public class EmergencyContactController extends Activity {
         return false;
     }
 
-    private void showMessage(String message){
+    /**
+     * This method is used to show a message to the user in the screen.
+     * @param message
+     */
+
+    private void showMessage(String message) {
 
         Toast.makeText(this,""+message,Toast.LENGTH_SHORT).show();
     }
 
-    private void disableOptions(Button save, Button update,Button delete){
+    /** CONFUSED METHOD
+     * This method is used to disable save,update and delete buttons on the field of
+     * given emergency contact.
+     *
+     * @param save
+     * @param update
+     * @param delete
+     */
+
+    private void disableOptions(Button save, Button update,Button delete) {
 
         save.setVisibility(View.VISIBLE);
         update.setVisibility(View.INVISIBLE);
         delete.setVisibility(View.INVISIBLE);
     }
 
-    private void disableField(Button save,EditText name, EditText phone){
+    /**
+     * This method is used to disable an emergency contact field.
+     *
+     * @param save
+     * @param name
+     * @param phone
+     */
+
+    private void disableField(Button save,EditText name, EditText phone) {
 
         save.setVisibility(View.INVISIBLE);
         name.setEnabled(false);
         phone.setEnabled(false);
     }
-    private void disableButtons(Button save,Button update,Button delete){
+
+    /** CONFUSED METHOD
+     * This method disable only save button.
+     *
+     *
+     * @param save
+     * @param update
+     * @param delete
+     */
+
+    private void disableButtons(Button save,Button update,Button delete) {
 
         save.setVisibility(View.INVISIBLE);
         update.setVisibility(View.VISIBLE);
@@ -514,7 +604,16 @@ public class EmergencyContactController extends Activity {
         delete.setEnabled(true);
     }
 
-    private void disableJustUpdateButton(EditText name, EditText phone, Button update, Button save) {
+    /**
+     * This method disable only update button
+     *
+     * @param name
+     * @param phone
+     * @param update
+     * @param save
+     */
+
+    private void disableUpdateButton(EditText name, EditText phone, Button update, Button save) {
 
         name.setEnabled(true);
         phone.setEnabled(true);
@@ -523,7 +622,15 @@ public class EmergencyContactController extends Activity {
         save.setEnabled(true);
     }
 
-    private void disableOptionsUpdate(Button save,Button update,Button delete){
+    /**
+     * This method disable all save button.
+     *
+     * @param save
+     * @param update
+     * @param delete
+     */
+
+    private void disableSaveButton(Button save, Button update, Button delete) {
 
         save.setEnabled(false);
         update.setVisibility(View.VISIBLE);
@@ -532,25 +639,51 @@ public class EmergencyContactController extends Activity {
         delete.setEnabled(true);
     }
 
-    private void setOptionsVisible(Button save, Button update){
+    /**
+     * This method is used after the user is already signed in the system, and user will try to
+     * update it. It only will have the update button visible.
+     *
+     * @param save
+     * @param update
+     */
+
+    private void setOptionsVisible(Button save, Button update) {
 
         update.setVisibility(View.VISIBLE);
         save.setVisibility(View.INVISIBLE);
     }
 
-    private void disableField(EditText name,EditText phone){
+    /** Confusing name
+     * This method is used to disable name and phone field.
+     *
+     * @param name
+     * @param phone
+     */
+
+    private void disableField(EditText name,EditText phone) {
 
         name.setEnabled(false);
         phone.setEnabled(false);
     }
 
-    public void open_search(View mapScreen){
+    /**
+     * This method is used to open search screen, after clicking the search button.
+     *
+     * @param mapScreen
+     */
+
+    public void openSearch(View mapScreen) {
 
         Intent openSearch = new Intent();
         openSearch.setClass(this , SearchUsController.class);
         startActivity(openSearch);
         finish();
     }
+
+    /**
+     * This method is activated when user clicks in GO button, tracing a route to the closest
+     * health unity.
+     */
 
     public void goClicked(View map_screen) throws IOException, JSONException {
 
@@ -562,13 +695,23 @@ public class EmergencyContactController extends Activity {
         finish();
     }
 
-    public void listMapsImageClicked(View map_screen){
+    /**
+     * This method list all the USs, by proximity of the user location, after the list button
+     * is clicked.
+     */
+
+    public void listMapsImageClicked(View map_screen) {
 
         Intent listOfHealth = new Intent();
         listOfHealth.setClass(this , ListOfHealthUnitsController.class);
         startActivity(listOfHealth);
         finish();
     }
+
+    /**
+     * This method is activated when user is already in the configuration screen, and
+     * try to open it again.
+     */
 
     public void openConfig(View map_screen) {
 
@@ -577,11 +720,16 @@ public class EmergencyContactController extends Activity {
         startActivity(config);
     }
 
-    public void openMap(View mapScreen){
+    /**
+     * This method is activated when user clicks in the map button, and open a new map.
+     */
+
+    public void openMap(View mapScreen) {
 
         Intent mapActivity = new Intent();
         mapActivity.setClass(this, MapScreenController.class);
         startActivity(mapActivity);
         finish();
     }
+
 }
