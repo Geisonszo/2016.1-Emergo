9998a89f04f93d4a22c302985239a0ac62e068b8
Gabriel Rodrigues <gabrielrodriguesfaria97@gmail.com>
2016-09-14 14:25:45 -0300

    Applying new design technique to EmergencyContact

--- EmerGo/app/src/main/java/unlv/erc/emergo/model/EmergencyContact.java
+++ EmerGo/app/src/main/java/unlv/erc/emergo/model/EmergencyContact.java
@@ -8,65 +8,65 @@ package unlv.erc.emergo.model;
 
 public class EmergencyContact {
 
-    private String name;
-    private String phone;
+  private String name;
+  private String phone;
 
 
-    /**
-     * Empty constructor
-     */
-    public EmergencyContact() {
+  /**
+   * Empty constructor.
+   */
+  public EmergencyContact() {
 
-    }
+  }
 
 
-    /**
-     * Emergency contacts constructor
-     * @param name
-     * @param phone
-     */
-    public EmergencyContact(String name, String phone) {
-        setName(name);
-        setPhone(phone);
-    }
+  /**
+   * Emergency contact constructor.
+   * @param name
+   * @param phone
+   */
+  public EmergencyContact(String name, String phone) {
+    setName(name);
+    setPhone(phone);
+  }
 
-    /**
-     * Get the name of this emergency contact instance.
-     *
-     * @return name;
-     */
-    public String getName() {
+  /**
+   * Get the name of this emergency contact instance.
+   *
+   * @return name;
+   */
+  public String getName() {
 
-        return name;
-    }
+    return name;
+  }
 
-    /**
-     * Set the received name as name of this emergency contact instance.
-     *
-     * @param name
-     */
-    public void setName(String name) {
+  /**
+   * Set the received name as name of this emergency contact instance.
+   *
+   * @param name
+   */
+  public void setName(String name) {
 
-        this.name = name;
-    }
+    this.name = name;
+  }
 
-    /**
-     * Get the phone of this emergency contact instance.
-     *
-     * @return phone;
-     */
-    public String getPhone() {
+  /**
+   * Get the phone of this emergency contact instance.
+   *
+   * @return phone;
+   */
+  public String getPhone() {
 
-        return phone;
-    }
+    return phone;
+  }
 
-    /**
-     * Set the received phone as phone of this emergency contact instance.
-     *
-     * @param phone
-     */
-    public void setPhone(String phone) {
+   /**
+    * Set the received phone as phone of this emergency contact instance.
+    *
+    * @param phone
+    */
+  public void setPhone(String phone) {
 
-        this.phone = phone;
-    }
+    this.phone = phone;
+  }
 }
