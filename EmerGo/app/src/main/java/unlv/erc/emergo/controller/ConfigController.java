804f16118e0ec3756406afefd2cb755147a7140d
Gabriel Rodrigues <gabrielrodriguesfaria97@gmail.com>
2016-09-14 11:45:37 -0300

    Applying new design techniques in ConfigController

--- EmerGo/app/src/main/java/unlv/erc/emergo/controller/ConfigController.java
+++ EmerGo/app/src/main/java/unlv/erc/emergo/controller/ConfigController.java
@@ -22,182 +22,181 @@ import unlv.erc.emergo.R;
 
 public class ConfigController extends Activity {
 
-    private Button emergencyContact;
-    private ImageView iconList, buttonGo;
-    private Button medicalRecord;
-    private Button aboutApp;
+  private Button emergencyContact;
+  private ImageView iconList;
+  private ImageView buttonGo;
+  private Button medicalRecord;
+  private Button aboutApp;
 
-    final String MESSAGE = "Está tentando abrir a página atual";
-    final String ROUTETRACED = "Rota mais próxima traçada";
+  final String MESSAGE = "Está tentando abrir a página atual";
+  final String ROUTETRACED = "Rota mais próxima traçada";
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
 
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.config);
-        iconList = (ImageView) findViewById(R.id.iconList);
-        buttonGo = (ImageView) findViewById(R.id.buttonGo);
-        emergencyContact = (Button) findViewById(R.id.emergencyContact);
-        medicalRecord = (Button) findViewById(R.id.medicalRecords);
-        aboutApp = (Button) findViewById(R.id.app);
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.config);
+    iconList = (ImageView) findViewById(R.id.iconList);
+    buttonGo = (ImageView) findViewById(R.id.buttonGo);
+    emergencyContact = (Button) findViewById(R.id.emergencyContact);
+    medicalRecord = (Button) findViewById(R.id.medicalRecords);
+    aboutApp = (Button) findViewById(R.id.app);
 
 
-        buttonGo.setOnClickListener(new View.OnClickListener() {
+    buttonGo.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
                 createRouteScreen();
             }
         });
-
-        iconList.setOnClickListener(new View.OnClickListener() {
+    iconList.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
                 listUs();
             }
         });
-
-        medicalRecord.setOnClickListener(new View.OnClickListener() {
+    medicalRecord.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View v) {
                 showMedicalRecord();
             }
         });
-
-        emergencyContact.setOnClickListener(new View.OnClickListener() {
+    emergencyContact.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View v) {
                 acessEmergencyContact();
             }
         });
-
-        aboutApp.setOnClickListener(new View.OnClickListener() {
+    aboutApp.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View v) {
                 seeInfoAboutApp();
             }
         });
+  }
 
-    }
+  /**
+   * This method create a route screen after GO button is clicked, tracing a route to a
+   * specific health unity.
+   */
+
+  public void createRouteScreen() {
+        
+    Intent map = new Intent();
+    map.setClass(this , RouteActivity.class);
+    map.putExtra("numeroUs" , -1);
+    startActivity(map);
+    finish();
+  }
+
+  /**
+   * This method launchs the search screen after the search button is clicked.
+   * @param mapScreen
+   */
+
+  public void openSearch(View mapScreen) {
+    Intent openSearch = new Intent();
+    openSearch.setClass(this , SearchUsController.class);
+    startActivity(openSearch);
+    finish();
+  }
 
     /**
-     * This method create a route screen after GO button is clicked, tracing a route to a
-     * specific health unity.
-     */
-
-    public void createRouteScreen() {
-        Intent map = new Intent();
-        map.setClass(this , RouteActivity.class);
-        map.putExtra("numeroUs" , -1);
-        startActivity(map);
-        finish();
-    }
-
-    /*
-     * This method launchs the search screen after the search button is clicked.
-     */
-
-    public void openSearch(View mapScreen) {
-        Intent openSearch = new Intent();
-        openSearch.setClass(this , SearchUsController.class);
-        startActivity(openSearch);
-        finish();
-    }
-
-    /*
      * This method list all the USs, by proximity of the user location, after the list button
      * is clicked.
      */
 
-    public void listUs() {
+  public void listUs() {
 
-        Intent listUs = new Intent();
-        listUs.setClass(this , ListOfHealthUnitsController.class);
-        startActivity(listUs);
-        finish();
-    }
+    Intent listUs = new Intent();
+    listUs.setClass(this , ListOfHealthUnitsController.class);
+    startActivity(listUs);
+    finish();
+  }
 
-    /*
-     * This method show users medical information, after the medical information button is clicked.
-     */
+  /**
+   * This method show users medical information, after the medical information button is clicked.
+   */
 
-    public void showMedicalRecord() {
+  public void showMedicalRecord() {
 
-        Intent medicalRecord = new Intent();
-        medicalRecord.setClass(this,MedicalRecordsController.class);
-        startActivity(medicalRecord);
-    }
+    Intent medicalRecord = new Intent();
+    medicalRecord.setClass(this,MedicalRecordsController.class);
+    startActivity(medicalRecord);
+  }
 
-    /*
-     * This method show user's emergency contacts, after the emergency contact button is clicked.
-     */
+  /**
+   * This method show user's emergency contacts, after the emergency contact button is clicked.
+   */
 
-    public void acessEmergencyContact() {
+  public void acessEmergencyContact() {
 
-        Intent emergencyContact = new Intent();
-        emergencyContact.setClass(this,EmergencyContactController.class);
-        startActivity(emergencyContact);
-    }
+    Intent emergencyContact = new Intent();
+    emergencyContact.setClass(this,EmergencyContactController.class);
+    startActivity(emergencyContact);
+  }
 
-    /*
-     * This method show the info related to EmerGo, after the button about is clicked.
-     */
+  /**
+   * This method show the info related to EmerGo, after the button about is clicked.
+   */
 
-    public void seeInfoAboutApp() {
+  public void seeInfoAboutApp() {
 
-        Intent aboutApp = new Intent();
-        aboutApp.setClass(this,AboutApp.class);
-        startActivity(aboutApp);
-    }
+    Intent aboutApp = new Intent();
+    aboutApp.setClass(this,AboutApp.class);
+    startActivity(aboutApp);
+  }
 
-    /*
-     * This method is activated when user clicks in GO button, tracing a route to the closest
-     * health unity.
-     */
+  /**
+   * This method is activated when user clicks in GO button, tracing a route to the closest
+   * health unity.
+   */
 
-    public void goClicked(View map_screen) throws IOException, JSONException {
+  public void goClicked(View mapScreen) throws IOException, JSONException {
 
-        Toast.makeText(this, ROUTETRACED , Toast.LENGTH_SHORT).show();
-        Intent routeActivity = new Intent();
-        routeActivity.setClass(ConfigController.this , RouteActivity.class);
-        routeActivity.putExtra("numeroUs" , -1);
-        startActivity(routeActivity);
-        finish();
-    }
+    Toast.makeText(this, ROUTETRACED , Toast.LENGTH_SHORT).show();
+    Intent routeActivity = new Intent();
+    routeActivity.setClass(ConfigController.this , RouteActivity.class);
+    routeActivity.putExtra("numeroUs" , -1);
+    startActivity(routeActivity);
+    finish();
+  }
 
-    /*
-     * This method list all the USs, by proximity of the user location, after the list button
-     * is clicked.
-     */
+  /**
+   * This method list all the USs, by proximity of the user location, after the list button
+   * is clicked.
+   */
 
-    public void listMapsImageClicked(View map_screen) {
+  public void listMapsImageClicked(View mapScreen) {
 
-        Intent listOfHealth = new Intent();
-        listOfHealth.setClass(this , ListOfHealthUnitsController.class);
-        startActivity(listOfHealth);
-        finish();
-    }
+    Intent listOfHealth = new Intent();
+    listOfHealth.setClass(this , ListOfHealthUnitsController.class);
+    startActivity(listOfHealth);
+    finish();
+  }
 
-    /*
-     * This method is activated when user is already in the configuration screen, and
-     * try to open it again.
-     */
+  /**
+   * This method is activated when user is already in the configuration screen, and
+   * try to open it again.
+   */
 
-    public void openConfig(View map_screen) {
+  public void openConfig(View mapScreen) {
 
-        Toast.makeText(this , MESSAGE , Toast.LENGTH_SHORT ).show();
-    }
+    Toast.makeText(this , MESSAGE , Toast.LENGTH_SHORT ).show();
+  }
 
-    /*
-     * This method is activated when user clicks in the map button, and open a new map.
-     */
+  /**
+   * This method is activated when user clicks in the map button, and open a new map.
+   */
 
-    public void openMap(View mapScreen) {
+  public void openMap(View mapScreen) {
 
-        Intent mapActivity = new Intent();
-        mapActivity.setClass(this, MapScreenController.class);
-        startActivity(mapActivity);
-        finish();
-    }
+    Intent mapActivity = new Intent();
+    mapActivity.setClass(this, MapScreenController.class);
+    startActivity(mapActivity);
+    finish();
+  }
+
 }
