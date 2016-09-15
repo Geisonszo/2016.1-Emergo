eb01e1b3bd6a0b09b25d43400f9da646c4b9202e
geisonszo <geisonszo@gmail.com>
2016-09-15 09:57:06 -0300

    Applying Design and style technique.

--- EmerGo/app/src/main/java/unlv/erc/emergo/controller/SearchUsController.java
+++ EmerGo/app/src/main/java/unlv/erc/emergo/controller/SearchUsController.java
@@ -15,151 +15,154 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import unlv.erc.emergo.R;
 import unlv.erc.emergo.model.HealthUnit;
 
-public class SearchUsController extends AppCompatActivity
-        implements SearchView.OnQueryTextListener, View.OnClickListener {
+import java.util.ArrayList;
+import java.util.List;
 
-    private SearchView mSearchView;
-    private ImageView goButton;
-    private ImageView map;
-    private ImageView usList;
-    private List<String> searchUss = new ArrayList<>();
-    private int numberOfUsClicked;
-    private ListView uSsList;
-    private CharSequence search;
-    ArrayList<String> closestsUs;
-    ArrayList<HealthUnit> abc;
 
-    public ListView getuSsList() {
-        return uSsList;
+
+public class SearchUsController extends AppCompatActivity implements SearchView
+    .OnQueryTextListener, View.OnClickListener {
+
+  private SearchView mSearchView;
+  private ImageView goButton;
+  private ImageView map;
+  private ImageView usList;
+  private List<String> searchUss = new ArrayList<>();
+  private int numberOfUsClicked;
+  private ListView uSsList;
+  private CharSequence search;
+  ArrayList<String> closestsUs;
+  ArrayList<HealthUnit> abc;
+
+  public ListView getuSsList() {
+
+    return uSsList;
+  }
+
+  public SearchView getmSearchView() {
+
+    return mSearchView;
+  }
+
+  public void setmSearchView(SearchView mSearchView) {
+
+    this.mSearchView = mSearchView;
+  }
+
+  public void openInformationUsScreen() {
+
+    Intent informationScreen = new Intent();
+
+    informationScreen.putExtra("position", numberOfUsClicked);
+    informationScreen.setClass(SearchUsController.this, InformationSearchScreenController.class);
+    startActivity(informationScreen);
+    finish();
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    super.onCreateOptionsMenu(menu);
+    goButton = (ImageView) findViewById(R.id.buttonGo);
+    goButton.setOnClickListener(this);
+    usList = (ImageView) findViewById(R.id.iconList);
+    usList.setOnClickListener(this);
+    map = (ImageView) findViewById(R.id.iconMap);
+    map.setOnClickListener(this);
+
+    //Carrega o arquivo de menu.
+    MenuInflater inflater = getMenuInflater();
+    inflater.inflate(R.menu.menu_search, menu);
+
+    //Pega o Componente.
+    mSearchView = (SearchView) menu.findItem(R.id.search)
+      .getActionView();
+
+    //Define um texto de ajuda:
+    mSearchView.setQueryHint("Busca");
+
+    // exemplos de utilização:
+    mSearchView.setOnQueryTextListener(this);
+
+    return true;
+  }
+
+  @Override
+  public void onClick(View v) {
+    if (v.getId() == R.id.buttonGo) {
+
+      Intent route = new Intent();
+
+      route.setClass(SearchUsController.this, RouteActivity.class);
+      startActivity(route);
+      finish();
     }
+    if (v.getId() == R.id.iconMap) {
 
-    public SearchView getmSearchView() {
-        return mSearchView;
+      Intent map = new Intent();
+
+      map.setClass(SearchUsController.this, MapScreenController.class);
+      map.putExtra("numeroUs" , -1);
+      startActivity(map);
+      finish();
     }
+    if (v.getId() == R.id.iconList) {
 
-    public void setmSearchView(SearchView mSearchView) {
-        this.mSearchView = mSearchView;
+      Intent list = new Intent();
+
+      list.setClass(SearchUsController.this, ListOfHealthUnitsController.class);
+      startActivity(list);
+      finish();
     }
+  }
 
-    public void openInformationUsScreen() {
+  public ArrayList<String> getSearchsUs(ArrayList<HealthUnit> closest) {
 
-        Intent informationScreen = new Intent();
-        
-        informationScreen.putExtra("position", numberOfUsClicked);
-        informationScreen.setClass(SearchUsController.this,
-                InformationSearchScreenController.class);
-        startActivity(informationScreen);
-        finish();
+    search = mSearchView.getQuery();
+    closestsUs = new ArrayList<>();
+    abc = new ArrayList<>();
+    int numberOfUs;
+
+    for (numberOfUs = 0 ; numberOfUs < HealthUnitController.getClosestsUs().size() ;
+        numberOfUs++) {
+      if (closest.get(numberOfUs).getNameHospital().toLowerCase().contains(search)) {
+        closestsUs.add(closest.get(numberOfUs ).getNameHospital());
+        abc.add(HealthUnitController.getClosestsUs().get(numberOfUs));
+      }
     }
+    return closestsUs;
+  }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        super.onCreateOptionsMenu(menu);
-        goButton = (ImageView) findViewById(R.id.buttonGo);
-        goButton.setOnClickListener(this);
-        usList = (ImageView) findViewById(R.id.iconList);
-        usList.setOnClickListener(this);
-        map = (ImageView) findViewById(R.id.iconMap);
-        map.setOnClickListener(this);
+  public void setuSsList(ListView uSsList) {
+    this.uSsList = uSsList;
+  }
 
-        //Carrega o arquivo de menu.
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_search, menu);
+  @Override
+  public boolean onQueryTextSubmit(String query) {
+    return false;
+  }
 
-        //Pega o Componente.
-        mSearchView = (SearchView) menu.findItem(R.id.search)
-                .getActionView();
+  @Override
+  public boolean onQueryTextChange(String newText) {
 
-        //Define um texto de ajuda:
-        mSearchView.setQueryHint("Busca");
+    searchUss = getSearchsUs(HealthUnitController.getClosestsUs());
 
-        // exemplos de utilização:
-        mSearchView.setOnQueryTextListener(this);
+    setuSsList((ListView) findViewById(R.id.list_of_search_us));
+    uSsList.setAdapter(new ArrayAdapter<>(this, R.layout.item, R.id.hospitalUnitText, searchUss));
+    uSsList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+      @Override
+      public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        numberOfUsClicked = parent.getPositionForView(view);
+        openInformationUsScreen();
+      }
+    });
+    return false;
+  }
 
-        return true;
-    }
-
-    @Override
-    public void onClick(View v) {
-        if (v.getId() == R.id.buttonGo) {
-
-            Intent route = new Intent();
-
-            route.setClass(SearchUsController.this, RouteActivity.class);
-            startActivity(route);
-            finish();
-        }
-        if (v.getId() == R.id.iconMap) {
-
-            Intent map = new Intent();
-
-            map.setClass(SearchUsController.this, MapScreenController.class);
-            map.putExtra("numeroUs" , -1);
-            startActivity(map);
-            finish();
-        }
-        if (v.getId() == R.id.iconList) {
-
-            Intent list = new Intent();
-
-            list.setClass(SearchUsController.this, ListOfHealthUnitsController.class);
-            startActivity(list);
-            finish();
-        }
-    }
-
-    public ArrayList<String> getSearchsUs(ArrayList<HealthUnit> closest){
-
-        search = mSearchView.getQuery();
-        closestsUs = new ArrayList<>();
-        abc = new ArrayList<>();
-        int numberOfUs;
-
-        for(numberOfUs = 0 ; numberOfUs < HealthUnitController.getClosestsUs().size() ;
-            numberOfUs++) {
-            if (closest.get(numberOfUs).getNameHospital().toLowerCase().contains(search)) {
-                closestsUs.add(closest.get(numberOfUs ).getNameHospital());
-                abc.add(HealthUnitController.getClosestsUs().get(numberOfUs));
-            }
-        }
-        return closestsUs;
-    }
-
-    public void setuSsList(ListView uSsList) {
-        this.uSsList = uSsList;
-    }
-
-    @Override
-    public boolean onQueryTextSubmit(String query) {
-        return false;
-    }
-
-    @Override
-    public boolean onQueryTextChange(String newText) {
-
-        searchUss = getSearchsUs(HealthUnitController.getClosestsUs());
-
-        setuSsList((ListView) findViewById(R.id.list_of_search_us));
-        uSsList.setAdapter(new ArrayAdapter<>(this, R.layout.item, R.id.hospitalUnitText,
-                searchUss));
-        uSsList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                numberOfUsClicked = parent.getPositionForView(view);
-                openInformationUsScreen();
-            }
-        });
-        return false;
-    }
-
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.search_screen);
-    }
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.search_screen);
+  }
 }
