adbff14f61ea30394a71e60027211e536649aecc
Gabriel Rodrigues <gabrielrodriguesfaria97@gmail.com>
2016-09-16 10:57:31 -0300

    Applying design technique to AboutApp

--- EmerGo/app/src/main/java/unlv/erc/emergo/controller/AboutApp.java
+++ EmerGo/app/src/main/java/unlv/erc/emergo/controller/AboutApp.java
@@ -8,21 +8,21 @@ import unlv.erc.emergo.R;
 
 public class AboutApp extends Activity {
 
-    private TextView informationAboutApp;
+  private TextView informationAboutApp;
 
-    @Override
-    protected void onCreate(final Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.about_app);
+  @Override
+  protected void onCreate(final Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.about_app);
 
-        String messageAboutApp = "\tEmerGo é um aplicativo que oferece a facilidade de " +
-                "encontrar Unidades de Saúde mais próximas. Possui MODO EMERGÊNCIA, " +
-                "que traça a rota para uma Unidade de Saúde mais próxima, " +
-                "ligar para o SAMU, e envia uma mensagem pré definida com pedido de ajuda para " +
-                "os contatos de emergência salvos!" +
-                "\n\n\tTodas as funcionalidades em suas mãos, em apenas um aplicativo.";
+    String messageAboutApp = "\tEmerGo é um aplicativo que oferece a facilidade de "
+                           + "encontrar Unidades de Saúde mais próximas. Possui MODO EMERGÊNCIA, "
+                           + "que traça a rota para uma Unidade de Saúde mais próxima, "
+            + "ligar para o SAMU, e envia uma mensagem pré definida com pedido de ajuda para "
+            + "os contatos de emergência salvos!"
+            + "\n\n\tTodas as funcionalidades em suas mãos, em apenas um aplicativo.";
 
-        informationAboutApp = (TextView) findViewById(R.id.aboutApp);
-        informationAboutApp.setText(messageAboutApp);
-    }
+    informationAboutApp = (TextView) findViewById(R.id.aboutApp);
+    informationAboutApp.setText(messageAboutApp);
+  }
 }
